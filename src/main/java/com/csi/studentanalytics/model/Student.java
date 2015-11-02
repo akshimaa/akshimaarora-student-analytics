e3108135297b3a0ceeefe5602dc75310aed7c351
Madan Parameswaran <madan.mesh@gmail.com>
2015-11-02 14:02:00 +1030

    REMOVE: name fields in Student.java

--- src/main/java/com/csi/studentanalytics/model/Student.java
+++ src/main/java/com/csi/studentanalytics/model/Student.java
@@ -14,9 +14,7 @@ import java.util.Locale;
  */
 public class Student {
     
-    private String firstName;
-    private String lastName;
-    private String middleName;
+
     private Date dateOfBirth;
     public enum Sex { MALE, FEMALE};
     private Sex gender;
@@ -34,35 +32,7 @@ public class Student {
     private float earnedGPA;
     private String highestEducation;
     private EquityData equityData;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-
-
-
-
+    
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
