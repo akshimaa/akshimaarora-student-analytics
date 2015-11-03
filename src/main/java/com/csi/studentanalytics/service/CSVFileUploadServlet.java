dd637d80ab1a0d975347df09e07b3e506cc65b3c
Madan Parameswaran <madan.mesh@gmail.com>
2015-11-03 13:42:06 +1030

    ADD: new empty java httpservlet for csv file upload and registered it in web.xml

---
+++ src/main/java/com/csi/studentanalytics/service/CSVFileUploadServlet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csi.studentanalytics.service;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ *
+ * @author madan
+ */
+public class CSVFileUploadServlet extends HttpServlet {
+    
+    @Override
+    public void init() throws ServletException
+    {
+        
+    }
+ 
+
+    @Override
+     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         
+    }
+}
