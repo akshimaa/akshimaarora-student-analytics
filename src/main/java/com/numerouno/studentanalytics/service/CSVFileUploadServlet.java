655b74d23f59445ba66c8744db1e4227f687d046
Madan Parameswaran <madan.mesh@gmail.com>
2015-11-12 00:31:32 +1030

    ADD: file download from s3 bucket. Testing waters here :)

--- src/main/java/com/numerouno/studentanalytics/service/CSVFileUploadServlet.java
+++ src/main/java/com/numerouno/studentanalytics/service/CSVFileUploadServlet.java
@@ -9,8 +9,9 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.numerouno.studentanalytics.controller.CSVParser;
+import com.numerouno.studentanalytics.processor.CSVParser;
 import java.io.File;
 import java.util.logging.Logger;
 import java.io.IOException;
@@ -20,6 +21,7 @@ import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 
 
 
@@ -43,15 +45,18 @@ public class CSVFileUploadServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         if(req != null)
         {
-            CSVParser.parseIntoPOJO(req.getPart("file").getInputStream());
-            Logger log = Logger.getLogger(CSVFileUploadServlet.class.getName());
-            log.info("CSV file parsed successfully!");
-            out.print("The file you uploaded is '" + req.getPart("file").getSubmittedFileName()+"' and it has been parsed successfully!");
-            
-        }
+//            CSVParser.parseIntoPOJO(req.getPart("file").getInputStream());
+//            Logger log = Logger.getLogger(CSVFileUploadServlet.class.getName());
+//            log.info("CSV file parsed successfully!");
+//            
+//            out.print("The file you uploaded is '" + req.getPart("file").getSubmittedFileName()+"' and it has been parsed successfully!");
+
+             }
         AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
-        AmazonS3 s3client = new AmazonS3Client(credentials);
-        s3client.putObject(new PutObjectRequest("student-alpha", "student.csv", 
-		new File("/Users/madan/Documents/student.csv")));
+            AmazonS3 s3client = new AmazonS3Client(credentials);
+            //s3client.putObject(new PutObjectRequest("student-alpha", "test.csv", new File("/Users/madan/Documents/student.csv")));
+            s3client.getObject(new GetObjectRequest("student-alpha","student.csv"), new File("/Users/madan/Desktop/student.csv"));
+                
+        
      }
 }
