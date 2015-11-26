46ac5b51b48ea884f41ef531a92bdfc2113befa6
Akshima Arora <akshima.arora@gmail.com>
2015-11-27 01:04:48 +1030

    Fix: fixed the js

--- src/main/webapp/js/pieChart.js
+++ src/main/webapp/js/pieChart.js
@@ -2,4 +2,14 @@
 //akshima
 
    
-
+ $('#generatePie').click(function(){
+        console.log("generate button clicked!");
+        console.log($('#datasourceDropdown').val());
+        console.log($('#presetDropdown').val());
+        datasource = $('#datasourceDropdown').val();
+        preset = $('#presetDropdown').val();
+        $('#datasource').attr('value',datasource);
+        $('#preset').attr('value', preset);
+        $('#pieChartForm').submit();
+         
+    }); 
