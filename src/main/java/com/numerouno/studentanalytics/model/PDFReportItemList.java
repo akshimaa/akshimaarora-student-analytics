5b3ef0a81bec14bdcb3d36df13cb642d3b3f1dd2
Sai Sumana <nsumana93@gmail.com>
2015-12-05 01:18:29 +1030

    Merge branch 'master' of github.com:mparameswaran/student-analytics
    # Conflicts:
    #	src/main/java/com/numerouno/studentanalytics/view/ReportGeneratorServlet.java

--- src/main/java/com/numerouno/studentanalytics/model/PDFReportItemList.java
+++ src/main/java/com/numerouno/studentanalytics/model/PDFReportItemList.java
@@ -9,16 +9,22 @@ import java.util.ArrayList;
 import org.ujmp.core.collections.list.ArrayIndexList;
 
 /**
- *
+ *PDFReportItemList generates the pdf report of the bar and pie charts
  * @author Akshima
  */
 public class PDFReportItemList {
     private static ArrayList<String> itemList = new ArrayIndexList<>();
-
+/**
+ * gets the item list to generate as pdf
+ * @return 
+ */
     public static ArrayList<String> getItemList() {
         return itemList;
     }
-
+/**
+ * sets the item list to generate pdf
+ * @param itemList sets the item list to generate pdf
+ */
     public static void setItemList(ArrayList<String> itemList) {
         PDFReportItemList.itemList = itemList;
     }
